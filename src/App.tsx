c5301be6351d782488287cb53ba9a9ce2b394161
AlwaysByrnes <141357497+AlwaysByrnes@users.noreply.github.com>
2024-07-15 18:49:29 -0400

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home"
+import { Blogs } from "./pages/Blogs"
+import { Navbar } from './components/navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="App">
+      <div className="app-container">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blogs" element={<Blogs />} />
+            <Route path="*" element={<h2> PAGE NOT FOUND </h2>} />
+        </Routes>
+      </Router>
+      </div>
     </div>
   );
 }
